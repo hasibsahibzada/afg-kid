0354e1f90e72a1940e811a82d6b0eb8f9f8f8bb5
hasibsahibzada <hbs@facelift-bbt.com>
2024-07-02 10:07:00 +0200

    integrated firebase deployement

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAE8ZRzy1nTqH89X14fBshm02biOq73tH4",
+    authDomain: "afghan-kid.firebaseapp.com",
+    projectId: "afghan-kid",
+    storageBucket: "afghan-kid.appspot.com",
+    messagingSenderId: "898482403192",
+    appId: "1:898482403192:web:4de3bd62ea9b73aec23a8d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
